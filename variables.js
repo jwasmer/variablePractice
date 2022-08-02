680c507868cc2b0b026e36ff1e3c44c2b8b325a9
James Wasmer <jameswasmer@gmail.com>
2022-08-02 17:31:38 -0400

    Add variables.js for push

---
+++ variables.js
@@ -0,0 +1,20 @@
+var stringFirst = "1"
+var stringSecond = "2"
+var stringThird = "3"
+
+console.log(stringFirst, stringSecond, stringThird)
+
+var numFirst = 1
+var numSecond = 2
+var numThird = 3
+
+var boolFirst = true
+var boolSecond = false
+var boolThird = true
+
+var stringFirst = "4"
+var stringSecond = "5"
+var stringThird = "6"
+
+console.log(stringFirst, stringSecond, stringThird)
+console.log(numFirst, numSecond, numThird)
