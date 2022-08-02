cbdb8ecacef354006b78cefe3c8bb21b6f8419ca
James Wasmer <jameswasmer@gmail.com>
2022-08-02 18:46:21 -0400

    Stage file

---
+++ secondAttempt.js
@@ -0,0 +1,20 @@
+var stringOne = "First"
+var stringTwo = "Second"
+var stringThree = "Third"
+
+console.log(stringOne, stringTwo, stringThree)
+
+var numOne = 1
+var numTwo = 2
+var numThree = 3
+
+var boolOne = true
+var boolTwo = true
+var boolThree = false
+
+var stringOne = "Fourth"
+var stringTwo = "Fifth"
+var stringThree = "Sixth"
+
+console.log(stringOne, stringTwo, stringThree)
+console.log(boolOne, boolTwo, boolThree)
